088057c58ddeb1e4b742b355fbfbdaa92d0665e3
christophertpow <33101910+christophertpow@users.noreply.github.com>
2017-10-26 16:46:08 -0500

    Initial Commit and Files Added
    Preliminary Commit for Assignment #1

---
+++ js/Assignment1.js
@@ -0,0 +1,19 @@
+function MenuChoice(selection)
+{
+    document.getElementById("area1").style.visibility = "hidden";
+    document.getElementById("area2").style.visibility = "hidden";
+
+switch (selection)
+{
+    case "Area 1":
+    document.getElementById("area1").style.visibility = "visible";
+    break;
+    case "Area 2":
+    document.getElementById("area2").style.visibility = "visible";
+    break;
+    case "None": //No menu item selected, so no section should be displayed
+    break;
+    default:
+        alert("Please select a different menu option");
+        }
+        }
